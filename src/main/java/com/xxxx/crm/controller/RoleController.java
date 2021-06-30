3a053374dd5473eb49965592073ced0a68fe0613
ms0121 <1132601565@qq.com>
2021-06-30 13:47:28 +0800

    1.1.1

--- src/main/java/com/xxxx/crm/controller/RoleController.java
+++ src/main/java/com/xxxx/crm/controller/RoleController.java
@@ -1,8 +1,10 @@
 package com.xxxx.crm.controller;
 
 import com.xxxx.crm.base.BaseController;
+import com.xxxx.crm.query.RoleQuery;
 import com.xxxx.crm.service.RoleService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -22,6 +24,11 @@ public class RoleController extends BaseController {
     @Resource
     private RoleService roleService;
 
+    /**
+     * 查询所有的角色
+     * @param userId
+     * @return
+     */
     @RequestMapping("queryAllRoles")
     @ResponseBody
     public List<Map<String, Object>> queryAllRoles(Integer userId){
@@ -29,5 +36,14 @@ public class RoleController extends BaseController {
         System.out.println(maps);
         return maps;
     }
+
+    @GetMapping("list")
+    @ResponseBody
+    public Map<String, Object> selectByParams(RoleQuery roleQuery){
+        Map<String, Object> map = roleService.queryByParamsForTable(roleQuery);
+        return map;
+    }
+
+
 
 }
---
+++ src/main/java/com/xxxx/crm/query/RoleQuery.java
@@ -0,0 +1,20 @@
+package com.xxxx.crm.query;
+
+import com.xxxx.crm.base.BaseQuery;
+
+/**
+ * @author lms
+ * @date 2021-06-30 - 13:14
+ */
+public class RoleQuery extends BaseQuery {
+
+    private String roleName;
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
