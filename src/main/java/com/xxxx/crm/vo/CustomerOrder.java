c2c8d8ed10ee6e880fb07a68fe6f6e89951f7c7a
ms0121 <1132601565@qq.com>
2021-07-05 18:00:50 +0800

    1.1.6

--- src/main/java/com/xxxx/crm/vo/CustomerOrder.java
+++ src/main/java/com/xxxx/crm/vo/CustomerOrder.java
@@ -1,5 +1,7 @@
 package com.xxxx.crm.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class CustomerOrder {
@@ -15,8 +17,10 @@ public class CustomerOrder {
 
     private Integer state;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createDate;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateDate;
 
     private Integer isValid;
