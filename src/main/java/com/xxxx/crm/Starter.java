5fbcfd9364e4000c2f027f27d6f3074e078c696f
ms0121 <1132601565@qq.com>
2021-08-29 19:40:10 +0800

    1.1.7

--- src/main/java/com/xxxx/crm/Starter.java
+++ src/main/java/com/xxxx/crm/Starter.java
@@ -3,6 +3,8 @@ package com.xxxx.crm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -12,8 +14,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @MapperScan("com.xxxx.crm.dao")  // 将该包下的所有的类加入到容器当中
 @EnableScheduling
-public class Starter {
+public class Starter extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Starter.class);
+    }
+
+    /*设置打包上线之后的web项目的启动入口, 重写继承父类SpringBootServletInitializer的方法configure即可*/
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Starter.class);
     }
 }
